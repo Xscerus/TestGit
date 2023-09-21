d74167e90caef17fd8bea6cf2f19aa39c8e18778
Xscerus <xscerus@gmail.com>
2023-09-21 14:02:42 +0200

    commit1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Mr. International");
+        double i = 3.65;
     }
 }
