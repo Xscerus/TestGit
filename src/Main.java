9f7622097e64a12ba92b0373f8408e6799a09bb5
Xscerus <xscerus@gmail.com>
2023-09-28 13:46:25 +0200

    commit3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.awt.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Mr. International");
-        double i = 3.65;
+        Scanner skan = new Scanner(System.in);
+        int liczbaA = 15;
+        int liczbaB = 4;
+        int sumaLiczb = liczbaA + liczbaB;
+
+        System.out.println("Wartość sumy wynosi " + liczbaA + liczbaB);
+
     }
 }
